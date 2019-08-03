49ee76b991adf27bb7aa8d7a88229e25670797eb
Darcy Manoel <darcman00@gmail.com>
2019-08-03 15:34:55 +0930

    Create sliding UI panels
    Just like they used to be.

---
+++ scriptsheet.js
@@ -0,0 +1,8 @@
+function slideleft(){
+	document.getElementById("left").classList.toggle("side");
+	document.getElementById("left").classList.toggle("slide");
+}
+function slideright(){
+	document.getElementById("right").classList.toggle("side");
+	document.getElementById("right").classList.toggle("slide");
+}
