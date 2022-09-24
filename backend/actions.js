8e17778d8a48f05d2536017fcd6615fc9c9bd955
Darcy Manoel <darcman00@gmail.com>
2022-09-24 16:54:17 +0930

    Remove spacing from hardpoints list if there are empty groups

--- backend/actions.js
+++ backend/actions.js
@@ -59,61 +59,62 @@ function toggleMirror(){
 function addHardpoint(type){
 	switch(type){
 		case `gun`:
-			hardpoints[0].push([`\tgun`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[0].push([`\tgun`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[0].push([`\tgun`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[0].push([`\tgun`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 		case `turret`:
-			hardpoints[1].push([`\tturret`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[1].push([`\tturret`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[1].push([`\tturret`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[1].push([`\tturret`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 		case `fighter`:
-			hardpoints[2].push([`\tbay "fighter"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[2].push([`\tbay "fighter"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[2].push([`\tbay "fighter"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[2].push([`\tbay "fighter"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 		case `drone`:
-			hardpoints[2].push([`\tbay "drone"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[2].push([`\tbay "drone"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[2].push([`\tbay "drone"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[2].push([`\tbay "drone"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 		case `engine`:
-			hardpoints[3].push([`\tengine`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[3].push([`\tengine`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[3].push([`\tengine`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[3].push([`\tengine`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 		case `left`:
-			hardpoints[3].push([`\t"steering engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
-			hardpoints[3].push([`\t\tangle 90`])
-			hardpoints[3].push([`\t\tleft`])
+			hardpoints[3].push([`\t"steering engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n\t\tangle 90\n\t\tleft\n`])
 			break
 		case `right`:
-			hardpoints[3].push([`\t"steering engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
-			hardpoints[3].push([`\t\tangle 270`])
-			hardpoints[3].push([`\t\tright`])
+			hardpoints[3].push([`\t"steering engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n\t\tangle 270\n\t\tright\n`])
 			break
 		case `reverse`:
-			hardpoints[3].push([`\t"reverse engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2])
+			hardpoints[3].push([`\t"reverse engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			if(mirror){
-				hardpoints[3].push([`\t"reverse engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2])
+				hardpoints[3].push([`\t"reverse engine"`,Math.round((xCoordinate*(inflation*scale))*2)/2*-1,Math.round((yCoordinate*(inflation*scale))*2)/2+`\n`])
 			}
 			break
 	}
 	console.log(hardpoints)
-	document.getElementById(`hardpoints`).innerHTML=
-		hardpoints[0].map(e=>e.join(` `)).join(`\n`)+
-		`\n`+
-		hardpoints[1].map(e=>e.join(` `)).join(`\n`)+
-		`\n`+
-		hardpoints[2].map(e=>e.join(` `)).join(`\n`)+
-		`\n`+
-		hardpoints[3].map(e=>e.join(` `)).join(`\n`)
+	document.getElementById(`hardpoints`).innerHTML=``
+	if(hardpoints[0].length){
+		document.getElementById(`hardpoints`).innerHTML+=hardpoints[0].map(e=>e.join(` `)).join(``)
+	}
+	if(hardpoints[1].length){
+		document.getElementById(`hardpoints`).innerHTML+=hardpoints[1].map(e=>e.join(` `)).join(``)
+	}
+	if(hardpoints[2].length){
+		document.getElementById(`hardpoints`).innerHTML+=hardpoints[2].map(e=>e.join(` `)).join(``)
+	}
+	if(hardpoints[3].length){
+		document.getElementById(`hardpoints`).innerHTML+=hardpoints[3].map(e=>e.join(` `)).join(``)
+	}
 }
 function lockXAxis(){
 	if(xAxisLocked){
@@ -143,10 +144,9 @@ function lockYAxis(){
 }
 function copyPoints(){
 	navigator.clipboard.writeText(
-		hardpoints[0].map(e=>e.join(` `)).join(`\n`)+
-		`\n`+
-		hardpoints[1].map(e=>e.join(` `)).join(`\n`)+
-		`\n`+
-		hardpoints[2].map(e=>e.join(` `)).join(`\n`)
+		hardpoints[0].map(e=>e.join(` `)).join(``)+
+		hardpoints[1].map(e=>e.join(` `)).join(``)+
+		hardpoints[2].map(e=>e.join(` `)).join(``)+
+		hardpoints[3].map(e=>e.join(` `)).join(``)
 	)
 }
